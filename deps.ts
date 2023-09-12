e3107ba11f72483b3eddd3c3bb1db5259f06e20c
RJ <rongjihuang@gmail.com>
2023-09-12 17:03:16 +0800

    Init deno config

---
+++ deps.ts
@@ -0,0 +1,18 @@
+// Copyright 2023 the NextRJ organization. All Rights Reserved. MIT license.
+
+// deno/std
+export {
+  assert,
+  assertEquals,
+  assertFalse,
+  assertObjectMatch,
+  assertRejects,
+  assertStrictEquals,
+  assertThrows,
+} from 'https://deno.land/std@0.201.0/assert/mod.ts'
+
+// deno/x
+
+// npm
+
+// project
