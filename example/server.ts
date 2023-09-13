81c3983159023081ae9e9b2c182c049a21802e84
RJ <rongjihuang@gmail.com>
2023-09-13 10:38:15 +0800

    Add example server to use Route

---
+++ example/server.ts
@@ -0,0 +1,13 @@
+import Route from '../mod.ts'
+
+// init route
+const route = new Route()
+  .post('/', async (req) => new Response(await req.text()))
+  .get('/', () => new Response('Hello World!'))
+  .get('/:id', (_req, { id }) => new Response(id))
+  // request `static/m1/index.html`, pathParams[0]=m1/index.html
+  .get('/static/*', (_req, pathParams) => new Response(pathParams[0]))
+
+// start server
+const port = Deno.args.length ? parseInt(Deno.args[0]) : 8001
+Deno.serve({ port }, (req) => route.handle(req))
